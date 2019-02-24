ff9e95823a3d12c117c8195a482198985dc64534
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2019-02-24 19:39:37 +0900

    Use aws-sdk

--- test/test.js
+++ test/test.js
@@ -1,23 +1,15 @@
 const test = require('tape');
 const { packageTemplate } = require('..');
-const { makeSigner } = require('aws4-with-assume-role');
 
 test('test', t => {
   t.plan(2);
 
-  makeSigner((err, sign) => {
-    if (err) {
-      throw err;
-    }
-
-    packageTemplate({
-      bucket: 'node-tmp',
-      prefix: 'cfn-package/test',
-      templateFile: `${__dirname}/template.yaml`,
-      sign,
-    }, (err, template) => {
-      t.error(err);
-      t.ok(template.Resources.file.Properties.CodeUri.startsWith('s3://'));
-    });
+  packageTemplate({
+    bucket: 'node-tmp',
+    prefix: 'cfn-package/test',
+    templateFile: `${__dirname}/template.yaml`,
+  }, (err, template) => {
+    t.error(err);
+    t.ok(template.Resources.file.Properties.CodeUri.startsWith('s3://'));
   });
 });
